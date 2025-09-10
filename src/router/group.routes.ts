35380d860f80152e9b459a616537da24862ec133
Navin <ITdansaikul@gmail.com>
2025-09-10 17:53:47 +0700

    [EDIT]Re check Group controller model route

--- src/router/group.routes.ts
+++ src/router/group.routes.ts
@@ -3,8 +3,6 @@ import { GroupController } from "../controller/group.controller";
 
 export const groupRouter = new Hono();
 
-groupRouter.post("/createGroup/course/:courseId", GroupController.createGroup);
-
+groupRouter.post("/createGroup", GroupController.createGroup);
 groupRouter.get("/course/:courseId", GroupController.getAllGroups);
-
-groupRouter.patch("/course/:courseId/group/:groupId", GroupController.updateGroup);
+groupRouter.patch("/updateGroup", GroupController.updateGroup);
