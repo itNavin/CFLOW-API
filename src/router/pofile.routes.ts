116e8f0a2f642fcd2dfdb8d047a35df290ac4002
Navin <ITdansaikul@gmail.com>
2025-09-16 14:29:00 +0700

    [ADD] getProfileByUserId

--- src/router/pofile.routes.ts
+++ src/router/pofile.routes.ts
@@ -3,3 +3,4 @@ import { ProfileController } from "src/controller/profile.controller";
 
 export const profileRouter = new Hono();
 profileRouter.get("/getProfile", ProfileController.getProfile);
+profileRouter.get("/getProfileByUserId/:userId", ProfileController.getProfileByUserId);
