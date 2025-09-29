dffa9715118872b80614218eb0785e8db0369be3
Navin <ITdansaikul@gmail.com>
2025-09-29 15:53:26 +0700

    [ADD]solar user middleware.

--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -3,12 +3,6 @@ import { UserController } from "../controller/user.controller";
 
 export const userRouter = new Hono();
 
-// userRouter.post("/createUser", UserController.createUser);
-
-// userRouter.get("/", UserController.getUserById);
-
-// userRouter.get("/users", UserController.getAllUsers);
-
 userRouter.get(
   "/my-project/course/:courseId",
   UserController.getMyProjectByCourse
@@ -18,3 +12,13 @@ userRouter.post(
   "/upload-userData",
   UserController.uploadUserDataByExcel
 );
+
+userRouter.post("/create-lecturer", UserController.createLecturerUser);
+
+userRouter.post("/create-solar-lecturer", UserController.createSolarLecturerUser);
+
+userRouter.get("/all-users", UserController.getAllUsers);
+
+userRouter.post("/update-solar-password", UserController.updateSolarPassword);
+
+userRouter.get("/fetchStudentData", UserController.fetchStudentData);
